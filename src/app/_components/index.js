a241d43fb68b824888f734ad798f10db62564947
Zobam <zobamokoli7@gmail.com>
2024-02-05 11:01:23 +0100

    first commit

---
+++ src/app/_components/index.js
@@ -0,0 +1,7 @@
+export * from "./Contact";
+export * from "./Faq";
+export * from "./Footer";
+export * from "./Hero";
+export * from "./Navbar";
+export * from "./Services";
+export * from "./Testimonials";
